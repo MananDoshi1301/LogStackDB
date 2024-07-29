0b5b46c79778f7c7860b31c160f3a5804705e564
Manan Doshi <manan.doshi@colorado.edu>
2024-07-29 13:07:41 +0530

    Update on script for generating data

--- generate_data.sh
+++ generate_data.sh
@@ -1,11 +1,12 @@
 export TEST_DB_FILENAME=db_testdata.txt
 
 generate_data() {
-  # > "$TEST_DB_FILENAME"
+  > "$TEST_DB_FILENAME"
+  local num_iterations=$1
 
   words=("foo" "bar" "car" "apple" "banana")
 
-  for i in {1..20}; do
+  for ((i=1; i<=num_iterations; i++)); do
     random_word=${words[$RANDOM % ${#words[@]}]}
     echo "$random_word, $i" >> "$TEST_DB_FILENAME"
   done
@@ -16,7 +17,12 @@ clear_data() {
 }
 
 if [ "$1" = "add_data" ]; then
-  generate_data
+  if [ -n "$2" ]; then
+    generate_data "$2"
+    echo "Data Generation Complete!" 
+  else
+    echo "Please provide the number of iterations."
+  fi
 fi
 
 if [ "$1" = "clear_data" ]; then
